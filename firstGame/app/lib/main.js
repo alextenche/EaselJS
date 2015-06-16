d3eb8b5693b980f7e1dbb1c5a74ba97b3059306d
Alex Tenche <alex.tenche@gmail.com>
2015-06-16 12:07:49 +0300

    added firstGame

---
+++ firstGame/app/lib/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var utils = require('./utils'),
+  domReady = utils.domReady
+    , levels = require('./levels')
+    , Hero = require('./Hero')
+    , Diamond = require('./Diamond')
+    , Box = require('./Box')
+    , Block = require('./Block');
+
+var classMap = {
+    '1': Block,
+    '2': Diamond,
+    '3': Box,
+    '4': Hero
+};
+
+var c = createjs
+    , stage
+    , mapGroup;
+
+console.log('game started: EaselJS version:' + c.EaselJS.version);
+
+domReady(function init() {
+    prepareWorld();
+    stage.update();
+});
+
+
+function prepareWorld() {
+
+    stage = new c.Stage('main');
+    mapGroup = new c.Container();
+    mapGroup.x = 50;
+    mapGroup.y = 50;
+    stage.addChild(mapGroup);
+
+    var map = levels[0].map;
+    var tiles = [];
+
+    map.forEach(function (row, indexY) {
+        tiles.push([]);
+        row.forEach(function (tile, indexX) {
+            var TileClass = classMap[tile];
+            if (TileClass) {
+                var newTile = new TileClass(indexX, indexY);
+                mapGroup.addChild(newTile);
+            }
+            tiles[indexY][indexX] = newTile || null;
+        });
+    });
+
+    window.stage = stage;
+    window.testTiles = tiles;
+}
